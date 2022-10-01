96f4db168a5c46703f419abf62c116a3f917c892
gorkem-8 <buharali.gorkem@gmail.com>
2022-10-01 19:15:51 +0300

    player movement system

--- Collector 3D/Assets/Scripts/Player.cs
+++ Collector 3D/Assets/Scripts/Player.cs
@@ -7,27 +7,64 @@ public class Player : MonoBehaviour
 {
     private Rigidbody rb;
     private Camera cam;
+    private Vector3 oldTouchPosition;
+    private float speed = 5f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         cam = Camera.main;
+        oldTouchPosition = Vector3.zero;
     }
 
     void Update()
     {
         if (Touchscreen.current.primaryTouch.press.isPressed)
         {
-            Vector2 screenPosition= Touchscreen.current.primaryTouch.position.ReadValue();
-            Vector3 worldPosition = new Vector3(screenPosition.x,screenPosition.y,15f);
-            Vector3 world = cam.ScreenToWorldPoint(worldPosition);
-            Debug.Log(world);
-        }    
+            Vector2 screenPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+            Vector3 worldPosition = cam.ScreenToWorldPoint(new Vector3(screenPosition.x, screenPosition.y, 15f));
+            if (oldTouchPosition != Vector3.zero)
+            {
+                if (worldPosition.x - oldTouchPosition.x > 0.01f)
+                {
+                    Debug.Log(worldPosition + "fasfasf" + oldTouchPosition);
+                    MoveSides(true);
+                }
+
+                else if (worldPosition.x - oldTouchPosition.x < -0.01f)
+                {
+                    Debug.Log(worldPosition + "fasfasf" + oldTouchPosition);
+                    MoveSides(false);
+                }
+
+                else
+                    MoveForward();
+            }
+            oldTouchPosition = worldPosition;
+        }
+        else
+            MoveForward();
+
+        if (Touchscreen.current.primaryTouch.press.wasReleasedThisFrame)
+            oldTouchPosition = Vector3.zero;
+
     }
 
-    void FixedUpdate()
+    void MoveForward()
     {
-        rb.velocity = Vector3.forward * 5f;
-        
+        rb.velocity = Vector3.forward * speed;
     }
+
+    void MoveSides(bool right)
+    {
+
+        if (right)
+        {
+            rb.velocity = new Vector3(2f, 0f, 1f) * speed;
+        }
+        else
+            rb.velocity = new Vector3(-2f, 0f, 1f) * speed;
+    }
+
+
 }
